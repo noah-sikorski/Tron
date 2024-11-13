2757659c0705969d858f4f48a9567ed90f492bab
icyyam <143056093+icyyam@users.noreply.github.com>
2024-11-12 19:10:06 -0700

    exmem v2

--- Tron/exmem.v
+++ Tron/exmem.v
@@ -9,7 +9,7 @@ module exmem #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) (
 	
 	input we1, we2, clk,
 	
-	output reg  [(DATA_WIDTH-1):0] LED,
+	output reg [(DATA_WIDTH-1):0] LED,
 	output reg [(DATA_WIDTH-1):0] dataOut1,
 	output reg [(DATA_WIDTH-1):0] dataOut2
 );
@@ -25,50 +25,37 @@ reg [ADDR_WIDTH-1:0] addr2_reg;
 
 //enable IO
 wire IO;
-assign IO = (addr1[14] == 1'b1);//(ram[32768] == 16'b0000000000000001);
+assign IO = (addr1[14] == 1'b1); //(ram[32768] == 16'b0000000000000001);
 
 initial begin
 	$display("Loading memory");
-	$readmemh("C:\\intelFPGA_lite\\23.1std\\quartus\\bin64\\3710\\Tron\\Checkpoint3.dat", ram);
+	$readmemh("C:\\IntelQuartus\\23.1.1\\ece3710\\Tron\\Checkpoint3.dat", ram);
 	$display("done loading");
 end
 
 
 always @(posedge clk) begin
-
-		if (we1) 
-		begin
-			ram[addr1] <= dataIn1;
-			dataOut1 <= dataIn1;
-		end
-		else 
-		begin
-			dataOut1 <= ram[addr1];
-		end 
-	
-end
-
-always @(posedge clk) begin
-		if (we2) 
-		begin
-			ram[addr2] <= dataIn2;
-			dataOut2 <= dataIn2;
-		end
-		else 
-		begin
-			dataOut2 <= ram[addr2];
-		end  
-end
-
-always @(posedge clk) begin
-	if(IO) begin //If IO is enabled then write the computed value to the LED's
-		LED <= dataIn1;	
+	if (we1) begin
+		ram[addr1] <= dataIn1;
+		dataOut1 <= dataIn1;
+	end else begin
+		dataOut1 <= ram[addr1];
 	end 
 end
 
-//assign dataOut1 = ram[addr1_reg];
-//assign dataOut2 = ram[addr2_reg];
+always @(posedge clk) begin
+	if (we2) begin
+			ram[addr2] <= dataIn2;
+			dataOut2 <= dataIn2;
+	end else begin
+		dataOut2 <= ram[addr2];
+	end  
+end
 
-
+always @(posedge clk) begin
+	if (IO) begin //If IO is enabled then write the computed value to the LED's
+		LED <= dataIn1;	
+	end 
+end
 
 endmodule

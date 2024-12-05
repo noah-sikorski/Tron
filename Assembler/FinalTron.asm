1821af527907a45e96fbde1050274b164b49618b
noah-sikorski <sikorskin1@gmail.com>
2024-12-05 14:07:12 -0700

    Updated ASM for sound

--- Assembler/FinalTron.asm
+++ Assembler/FinalTron.asm
@@ -27,10 +27,12 @@ BNE .memEraseLoop
 
 LUI $234 %rF # Load the value 60000 into rF for audio addressing
 ORI $96  %rF
+
+LUI $64 %rE
+STOR %rE %rF # Stop
+
 MOVI $1 %rE
 STOR %rE %rF # Start sound
-MOVI $2 %rE
-STOR %rE %rF # Start Music
 
 MOVI $0 %rB
 
@@ -210,10 +212,10 @@ BNE .clearLoop
 
 LUI $234 %rF # Load the value 60000 into rF for audio addressing
 ORI $96  %rF
+
 LUI $64 %rE #Stop Code 15
 STOR %rE %rF # Stop
-MOVI $4 %rE
-STOR %rE %rF # Insert Coin
+
 LUI $128 %rE #Random Code 16
 STOR %rE %rF # Random SoundTrack
 
@@ -2892,10 +2894,10 @@ JUC  %rF
 .blueDied
 LUI $234 %rF # Load the value 60000 into rF for audio addressing
 ORI $96  %rF
+
 LUI $64 %rE
 STOR %rE %rF # Stop
-MOVI $8 %rE  # Sound Code 4
-STOR %rE %rF # Death Sound
+
 MOVI $16 %rE # Sound Code 5
 STOR %rE %rF # Winning Sound
 
@@ -3249,10 +3251,10 @@ JUC  %rF
 .yellowDied
 LUI $234 %rF # Load the value 60000 into rF for audio addressing
 ORI $96  %rF
-LUI $64  %rE
+
+LUI $64 %rE
 STOR %rE %rF # Stop
-MOVI $8  %rE # Sound Code 4
-STOR %rE %rF # Death Sound
+
 MOVI $16 %rE # Sound Code 5
 STOR %rE %rF # Winning Sound
 
